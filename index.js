2d8c73abfa6512acf9fd984cd26cd64d8fa104fb
Josh Murray <joshomurray@gmail.com>
2015-08-10 18:25:47 +0000

    Repair config

--- index.js
+++ index.js
@@ -7,41 +7,19 @@ var Storage = require( "storage.json" );
 /**
  * The instance of the AjaxStream module.
  */
-var stream;
+var stream = require( "./lib/stream" );
+
+/**
+ * Exporting the Stream instance.
+ * @type {stream|*|exports}
+ */
+module.exports = stream;
 
 /**
  * Creates a new configuation file if one does not yet exist.
  */
 var bootstrap = function(){
-    var config = {
-        def_env: "local",
-        local: {
-            host: "<Local CudaTel Address/IP>",
-            def_user: {
-                user: "<CudaTel Username>",
-                pass: "<CudaTel Password>"
-            }
-        },
-        remote: {
-            host: "<Remote CudaTel Address/IP>",
-            def_user: {
-                user: "<CudaTel Username>",
-                pass: "<CudaTel Password>"
-            }
-        }
-    };
 
-    var configFile = new Storage( "cudatel-ajax" );
-
-    configFile.load( null, function( loaded ){
-        if( Object.getOwnPropertyNames( loaded).length == 0 ) configFile.push( config, function( created ){
-            if( created[ "def_env" ]) console.log( "cudatel-ajax.json file created." );
-        });
-
-        stream = require( "./lib/stream" );
-
-        module.exports = stream;
-    });
 }
 
 bootstrap();
