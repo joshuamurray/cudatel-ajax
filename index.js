b1993905ef5f1d32e304b3d3e1ff147eda637f78
Josh Murray <joshomurray@gmail.com>
2015-08-11 13:24:19 +0000

    update storage.json

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 /**
- * Primary requirements
- * @type {Storage|exports}
- */
-var Storage = require( "storage.json" );
-
-/**
- * The instance of the AjaxStream module.
+ * Primary module requirements
  */
 var stream = require( "./lib/stream" );
+
+/**
+ * Called PRIOR TO export to ensure that all variable values from "config.json"
+ * have been loaded. Sets all initial variables to their respective values.
+ * Instantiates any modules requiring instantiation, prior to exporting.
+ */
+var bootstrap = function(){}
+
+/**
+ * Execute the bootstrap command before exporting
+ */
+bootstrap();
 
 /**
  * Exporting the Stream instance.
  * @type {stream|*|exports}
  */
 module.exports = stream;
-
-var bootstrap = function(){}
-
-bootstrap();
 
 
 
