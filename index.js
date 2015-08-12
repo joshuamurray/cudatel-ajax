5b810af830be63ed325360f4ff3be5ad2134c51a
Josh Murray <joshomurray@gmail.com>
2015-08-12 20:21:08 +0000

    1.1.6

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-/**
- * Primary module requirements
- */
-var stream = require( "./lib/stream" );
-
-/**
- * Called PRIOR TO export to ensure that all variable values from "config.json"
- * have been loaded. Sets all initial variables to their respective values.
- * Instantiates any modules requiring instantiation, prior to exporting.
- */
-var bootstrap = function(){}
-
-/**
- * Execute the bootstrap command before exporting
- */
-bootstrap();
-
-/**
- * Exporting the Stream instance.
- * @type {stream|*|exports}
- */
-module.exports = stream;
-
+module.exports = require( "./lib/Session" );
 
 
